13c5b60bcbcc56cba19056a28c7c915bf58bf735
Sandeep Bisht <>
2023-01-24 18:18:04 +0530

    auth api

--- api/Authantication/AuthRouting.js
+++ api/Authantication/AuthRouting.js
@@ -4,4 +4,9 @@ const checkauth= require('../../Midileware/checkauth')
 router.post("/register",AuthController.create);
 router.post("/login",AuthController.isuser)
 router.get(`/allusers`,AuthController.find_all)
+router.post("/user_by_id",AuthController.find_by_id)
+router.put("/update_user_by_id",AuthController.find_and_update)
+router.delete("/delete_user_by_id",AuthController.find_and_delete)
+
+
 module.exports = router;
