20927f4618a5105716207f85922e0cf77e160ba4
Sandeep Bisht <>
2023-03-29 12:01:06 +0530

    removed console and add public folder to gitignore

--- api/Authantication/AuthController.js
+++ api/Authantication/AuthController.js
@@ -4,13 +4,11 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   create: (req, res) => {
     const { password } = req.body;
-     console.log(req.body)
     bcrypt.hash(password, 10, (error, hash) => {
       if (error) {
         res.status(500).json({
           msg: "internal Server Error",
         });
-        console.log(error);
       } else {
         try {
           var data = {
@@ -22,9 +20,7 @@ module.exports = {
             userStatus:req.body.userStatus,
             organization:req.body.organization
           };
-          console.log(data, "insdide create dataaa")
           AuthService.create(data).then((result) => {
-            console.log(result,"created register")
             if (result) {              
               res.json({
                 sucess: 200,
@@ -110,16 +106,13 @@ module.exports = {
       var data = {
       username: req.body.username,
       };
-      // console.log(req.body);
       AuthService.isuser(data).then((result) => {
-        // console.log(result, 'resulttt')
         if (result) {          
           bcrypt.compare(
             req.body.password,
             result[0].password,
             (err, response) => {              
               if (response) {
-                // console.log(response, 'response')
                 var token = jwt.sign(
                   {
                     username: req.body.username,
@@ -127,7 +120,6 @@ module.exports = {
                   "this is my medzone key",
                   { expiresIn: "1h" }
                 )
-                // console.log('valid')
                 res.status(200).json({
                   token: token,                  
                   ...result[0]._doc
@@ -148,7 +140,6 @@ module.exports = {
         }
       });
     } catch (err) {
-      // console.log(err);
       res.json({
         sucess: 400,
         message: "Please provide correct information",
@@ -159,7 +150,6 @@ module.exports = {
   find_all: (req, res, next) => {
     try {            
       AuthService.find_all().then((result) => {
-        // console.log(result)
         if (result) {  
           res.status(200).json({
             data: result,
@@ -174,7 +164,6 @@ module.exports = {
         }
       });
     } catch (err) {
-      // console.log(err);
       res.json({
         sucess: 400,
         message: "Please provide correct information",
