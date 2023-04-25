6718b63694816145aebe953f45bab1ec092bee81
Sandeep Bisht <>
2023-04-25 18:25:06 +0530

    remove all code

--- api/Cart/CartRouting.js
+++
@@ -1,8 +0,0 @@
-const CartController = require("./CartController");
-const router = require("express").Router();
-const checkauth= require('../../Midileware/checkauth')
-router.post("/add_to_cart",CartController.create);
-router.post("/cart_by_id",CartController.find_by_id)
-router.put("/update_cart_by_id",CartController.find_and_update)
-router.delete("/delete_cart_by_id",CartController.find_and_delete)
-module.exports = router;
--- api/Subscribed/SubscribedRouting.js
+++
@@ -1,8 +0,0 @@
-const SubscribedController = require("./SubscribedController");
-const router = require("express").Router();
-const checkauth= require('../../Midileware/checkauth');
-const cors = require("cors");
-router.use(cors({ origin: true }));
-router.post("/subscribed",SubscribedController.create);
-router.post("/verify",SubscribedController.verify);
-module.exports = router;
