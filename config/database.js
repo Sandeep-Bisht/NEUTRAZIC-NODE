d88b5743150e24fe58f118d4c3c5912f5d654bf4
Sandeep-Bisht <sandeepbisht29@gmail.com>
2024-03-01 11:40:13 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -9,8 +9,10 @@ module.exports = function(){
         useCreateIndex: true,
          useFindAndModify: true,
         auth: {
-            "user":"NeutrazicAdmin",
-            "password":"SOSh3AWDPC4499GTbYhxY8",  
+            // "user":"NeutrazicAdmin",
+            // "password":"SOSh3AWDPC4499GTbYhxY8",  
+            "user":"Nutrazic_Giks",
+            "password":"Nutrazic1234",  
           },
     })
     mongoose.connection.on('connected',()=>{
